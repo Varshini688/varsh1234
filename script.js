33ace04069103007595cdc4347056e834608c99b
Varshini688 <varshiniajadagoudar@gmail.com>
2026-01-15 00:17:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const canvas = document.getElementById("confetti");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let confetti = [];
+
+function startCelebration() {
+    for (let i = 0; i < 200; i++) {
+        confetti.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height - canvas.height,
+            r: Math.random() * 6 + 2,
+            d: Math.random() * 5 + 2,
+            color: `hsl(${Math.random() * 360},100%,50%)`
+        });
+    }
+    animateConfetti();
+}
+
+function animateConfetti() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    confetti.forEach((c, i) => {
+        ctx.beginPath();
+        ctx.arc(c.x, c.y, c.r, 0, Math.PI * 2);
+        ctx.fillStyle = c.color;
+        ctx.fill();
+
+        c.y += c.d;
+        if (c.y > canvas.height) {
+            confetti[i].y = -10;
+        }
+    });
+
+    requestAnimationFrame(animateConfetti);
+}
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 
